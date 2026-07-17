bd9a802b48d3ae3be1abc984cc16e9bea48f3541
wudilsr <wudilsr@gmail.com>
2026-07-17 22:28:13 +0800

    soda: Remove duplicate sensors HAL config and update device.mk

--- device.mk
+++ device.mk
@@ -385,9 +385,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
