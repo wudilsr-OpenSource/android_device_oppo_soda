220a5a159a458ccd69be324dee2e2a862167fb02
wudilsr <wudilsr@gmail.com>
2026-08-08 20:45:47 +0800

    soda: Add gps.prop and copy to data partition
    - Add gps.prop to vendor/etc via PRODUCT_COPY_FILES
    - Copy gps.prop to /data/vendor/location on post-fs-data
    - Set ownership to gps:gps with 0660 permissions

--- device.mk
+++ device.mk
@@ -261,7 +261,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
+    $(LOCAL_PATH)/gps/gps.prop:$(TARGET_COPY_OUT_VENDOR)/etc/gps.prop
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
