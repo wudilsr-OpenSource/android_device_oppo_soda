54e10c8edf8033b65a1e8d42ba58cd496c5abd4d
wudilsr <wudilsr@gmail.com>
2026-08-09 22:29:23 +0800

    soda: Fix duplicate prop types and drop permissive boot arg
    - Drop vendor_internal_prop(vendor_camera_prop): qcom already defines
      vendor_restricted_prop(vendor_camera_prop); keep context mapping and
      set_prop(hal_camera_default) which is legal for vendor domains
    - Reuse qcom vendor_ssd_block_device instead of custom vendor_ssd_block_dev;
      remove custom ssd file_contexts rule and tee.te (qcom tee.te already
      grants rw to qseecomd)
    - Remove androidboot.selinux=permissive from kernel cmdline to validate
      denials under enforcing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ BOARD_KERNEL_CMDLINE := \
     service_locator.enable=1 \
     swiotlb=2048 \
     loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
